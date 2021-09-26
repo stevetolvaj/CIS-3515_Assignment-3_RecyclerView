a701d74bf6018321b29774e05a236c42a90ad824
SteveT90 <stevetolvaj@gmail.com>
2021-09-26 16:56:32 -0400

    Refactored MainActivity.kt to ImageActivity.kt

--- app/src/main/java/edu/temple/cis_3515_assignment_3_recyclerview/ImageActivity.kt
+++ app/src/main/java/edu/temple/cis_3515_assignment_3_recyclerview/ImageActivity.kt
@@ -3,7 +3,7 @@ package edu.temple.cis_3515_assignment_3_recyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
