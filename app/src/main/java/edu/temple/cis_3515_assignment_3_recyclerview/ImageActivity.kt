48ab6881a477888a922a37df701d7cf8783e1c8e
SteveT90 <stevetolvaj@gmail.com>
2021-09-26 17:21:02 -0400

    Layout set up

--- app/src/main/java/edu/temple/cis_3515_assignment_3_recyclerview/ImageActivity.kt
+++ app/src/main/java/edu/temple/cis_3515_assignment_3_recyclerview/ImageActivity.kt
@@ -2,10 +2,17 @@ package edu.temple.cis_3515_assignment_3_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // var mRecyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        // val numOfColumns = 2;
+        // mRecyclerView.layoutManager = GridLayoutManager(this, numOfColumns)
+
     }
 }
